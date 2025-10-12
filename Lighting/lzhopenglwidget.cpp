0181ddea5c398f31a79201c166383b6db155b8e8
lzh250 <lzh250@126.com>
2025-10-12 22:31:29 +0800

    设置材质-分别设置光属性的分量：环境光、光反射、镜面光

--- Lighting/lzhopenglwidget.cpp
+++ Lighting/lzhopenglwidget.cpp
@@ -79,31 +79,37 @@ const char *object_fragment_shader_source = R"(
         float shininess;
     };
 
+    struct Light {
+        vec3 position;
+
+        vec3 ambient;
+        vec3 diffuse;
+        vec3 specular;
+    };
+
     in vec3 fragPos;
     in vec3 normal;
 
-    uniform vec3 lightPos;
     uniform vec3 viewPos;
-    uniform vec3 lightColor;
-    uniform vec3 objectColor;
     uniform Material material;
+    uniform Light light;
 
     void main()
     {
         // ambient
-        vec3 ambient = vec3(0.1) * material.ambient;
+        vec3 ambient = light.ambient * material.ambient;
 
         // diffuse
         vec3 norm = normalize(normal);
-        vec3 lightDir = normalize(lightPos - fragPos);
+        vec3 lightDir = normalize(light.position - fragPos);
         float diff = max(dot(norm, lightDir), 0.0);
-        vec3 diffuse = lightColor * (diff * material.diffuse);
+        vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
         // specular
         vec3 viewDir = normalize(viewPos - fragPos);
         vec3 reflectDir = reflect(-lightDir, norm);
         float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-        vec3 specular = lightColor * (spec * material.specular);
+        vec3 specular = light.specular * (spec * material.specular);
 
         // 通过环境光照、漫反射关照、镜面光照得出结果
         vec3 result = ambient + diffuse + specular;
@@ -198,9 +204,14 @@ void LzhOpenGLWidget::initializeGL()
     }
 
     glUseProgram(object_program);
-    glUniform3f(glGetUniformLocation(object_program, "objectColor"), 1.0f, 0.5f, 0.31f);
-    glUniform3f(glGetUniformLocation(object_program, "lightColor"), 1.0f, 1.0f, 1.0f);
-    glUniform3f(glGetUniformLocation(object_program, "lightPos"), light_pos.x(), light_pos.y(), light_pos.z());
+
+    // light properties
+    glUniform3f(glGetUniformLocation(object_program, "light.position"), light_pos.x(), light_pos.y(), light_pos.z());
+    glUniform3f(glGetUniformLocation(object_program, "light.ambient"), 0.1f, 0.1f, 0.1f);
+    glUniform3f(glGetUniformLocation(object_program, "light.diffuse"), 0.5f, 0.5f, 0.5f);
+    glUniform3f(glGetUniformLocation(object_program, "light.specular"), 1.0f, 1.0f, 1.0f);
+
+    // material properties
     glUniform3f(glGetUniformLocation(object_program, "material.ambient"), 1.0f, 0.5f, 0.31f);
     glUniform3f(glGetUniformLocation(object_program, "material.diffuse"), 1.0f, 0.5f, 0.31f);
     glUniform3f(glGetUniformLocation(object_program, "material.specular"), 0.5f, 0.5f, 0.5f); // specular lighting doesn't have full effect on this object's material
@@ -237,7 +248,7 @@ void LzhOpenGLWidget::initializeGL()
     }
 
     glUseProgram(light_program);
-    glUniform3f(glGetUniformLocation(object_program, "lightPos"), light_pos.x(), light_pos.y(), light_pos.z());
+    glUniform3f(glGetUniformLocation(light_program, "lightPos"), light_pos.x(), light_pos.y(), light_pos.z());
     glUniform4f(glGetUniformLocation(light_program, "lightColor"), 1.0f, 1.0f, 1.0f, 1.0f);
 }
 
