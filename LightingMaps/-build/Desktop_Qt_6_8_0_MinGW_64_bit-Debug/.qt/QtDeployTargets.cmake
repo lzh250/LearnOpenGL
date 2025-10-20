acbd69f579ca3fcf6836536c8766fda1d2256cc2
lzh250 <lzh250@126.com>
2025-10-20 23:49:14 +0800

    增加工程 LightCasters

--- LightingMaps/-build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Lighting_FILE E:/lzh/study/OpenglQt6/github/Lighting/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Lighting.exe)
-set(__QT_DEPLOY_TARGET_Lighting_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Lighting_RUNTIME_DLLS D:/Qt/6.8.0/mingw_64/bin/Qt6OpenGLWidgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6OpenGL.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
--- LightingMaps/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_LightingMaps_FILE E:/lzh/study/OpenglQt6/github/LightingMaps/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/LightingMaps.exe)
-set(__QT_DEPLOY_TARGET_LightingMaps_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_LightingMaps_RUNTIME_DLLS D:/Qt/6.8.0/mingw_64/bin/Qt6OpenGLWidgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6OpenGL.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
