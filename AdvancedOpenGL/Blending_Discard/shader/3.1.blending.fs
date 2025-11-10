c5ebb4d8a23f5e65676b440150d922c20b546f55
lzh250 <lzh250@126.com>
2025-11-10 19:12:25 +0800

    Blending_Discard处理一下草有虚白框问题。这不是解决问题的办法，应付。

--- AdvancedOpenGL/Blending_Discard/shader/3.1.blending.fs
+++ AdvancedOpenGL/Blending_Discard/shader/3.1.blending.fs
@@ -10,5 +10,7 @@ void main()
     vec4 texColor = texture(texture1, TexCoords);
     if(texColor.a < 0.1)
         discard;
+    if (texColor.r > 0.5 && texColor.g > 0.5 && texColor.b > 0.5)
+        discard;
     FragColor = texColor;
 }
