40ce1422b6e738e98212db0419db8f90f2cedacf
lzh250 <lzh250@126.com>
2025-12-02 03:36:17 +0800

    增加工程 Cubemap_Skybox

---
+++ AdvancedOpenGL/6.1.Cubemaps_SkyBox/lzhopenglwidget.cpp
@@ -0,0 +1,434 @@
+#include "lzhopenglwidget.h"
+
+#include <QKeyEvent>
+
+float cube_vertices[] = {
+    // positions          // texture Coords
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+float skybox_vertices[] = {
+    // positions
+    -1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+
+    -1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+
+    -1.0f, -1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+
+    -1.0f,  1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f,
+
+    -1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f
+};
+
+
+LzhOpenGLWidget::LzhOpenGLWidget(QWidget *parent) :
+    QOpenGLWidget(parent)
+{
+    setFocusPolicy(Qt::StrongFocus);
+    setMouseTracking(true);
+}
+
+LzhOpenGLWidget::~LzhOpenGLWidget()
+{
+    makeCurrent();
+    glDeleteVertexArrays(1, &cube_VAO);
+    glDeleteVertexArrays(1, &skybox_VAO);
+    glDeleteBuffers(1, &cube_VBO);
+    glDeleteBuffers(1, &skybox_VBO);
+    doneCurrent();
+}
+
+void LzhOpenGLWidget::initializeGL()
+{
+    initializeOpenGLFunctions();
+
+    glEnable(GL_DEPTH_TEST);
+
+    cam_pos   = QVector3D(0.0f, 0.0f,  3.0f);
+    cam_front = QVector3D(0.0f, 0.0f, -1.0f);
+    cam_up    = QVector3D(0.0f, 1.0f,  0.0f);
+
+    shader.Init(":/shader/6.1.cubemaps.vs", ":/shader/6.1.cubemaps.fs");
+    skybox_shader.Init(":/shader/6.1.skybox.vs", ":/shader/6.1.skybox.fs");
+
+    //cube VAO
+    glGenVertexArrays(1, &cube_VAO);
+    glGenBuffers(1, &cube_VBO);
+    glBindVertexArray(cube_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cube_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+
+    // skybox VAO
+    glGenVertexArrays(1, &skybox_VAO);
+    glGenBuffers(1, &skybox_VBO);
+    glBindVertexArray(skybox_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skybox_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skybox_vertices), skybox_vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+
+    // load textures
+    cube_texture  = LoadTexture(":/res/container.jpg");
+
+    vector<std::string> faces
+    {
+        ":/res/skybox/right.jpg",
+        ":/res/skybox/left.jpg",
+        ":/res/skybox/top.jpg",
+        ":/res/skybox/bottom.jpg",
+        ":/res/skybox/front.jpg",
+        ":/res/skybox/back.jpg"
+    };
+    cubemap_texture = LoadCubemap(faces);
+
+    shader.Use();
+    shader.SetInt("texture1", 0);
+
+    skybox_shader.Use();
+    skybox_shader.SetInt("skybox", 0);
+}
+
+void LzhOpenGLWidget::resizeGL(int w, int h)
+{
+    Perspective();
+    shader.Use();
+    shader.SetMat4("projection", perspective);
+    skybox_shader.Use();
+    skybox_shader.SetMat4("projection", perspective);
+}
+
+void LzhOpenGLWidget::paintGL()
+{
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /*********************************************************************************
+     * 可通过先绘制天空盒子，再打开深度测试绘制箱子物体达到想要的结果，但为了优化一点点性能，
+     * 可通过后绘制天空盒子，减少带宽（片段着色器的调用次数），绘制时欺骗深度缓冲天空盒子的距离为最远
+     ********************************************************************************/
+
+    // 绘制箱子
+    shader.Use();
+    QMatrix4x4 model;
+    shader.SetMat4("model", model);
+    QMatrix4x4 view = LookAt(cam_pos, cam_pos + cam_front, cam_up);
+    shader.SetMat4("view", view);
+
+    glBindVertexArray(cube_VAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, cube_texture);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+
+    // 绘制 skybox
+    glDepthFunc(GL_LEQUAL); // change depth function so depth test passes when values are equal to depth buffer's content
+    skybox_shader.Use();
+
+    // 让skybox世界坐标位置跟随相机移动，视觉上skybox不移动只能旋转
+    view.setRow(3, QVector4D(0.0, 0.0, 0.0, 0.0));
+    view.setColumn(3, QVector4D(0.0, 0.0, 0.0, 0.0));
+
+    skybox_shader.SetMat4("view", view);
+    glBindVertexArray(skybox_VAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap_texture);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+    glDepthFunc(GL_LESS);
+}
+
+void LzhOpenGLWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    QPoint pos = event->pos();
+
+    if (first_mouse)
+    {
+        QCursor::setPos(mapToGlobal(QPoint(width() / 2, height() / 2)));
+        first_mouse = false;
+        return;
+    }
+
+    float x_offset = pos.x() - width()/2;
+    float y_offset = height()/2 - pos.y();
+
+    x_offset *= 0.05f;
+    y_offset *= 0.05f;
+
+    yaw   += x_offset;
+    pitch += y_offset;
+
+    if (pitch >  89.0f) pitch =  89.0f;
+    if (pitch < -89.0f) pitch = -89.0f;
+
+    QVector3D front;
+    // 默认朝向是世界X轴方向(不考虑俯仰)，yaw应该初始化为-90
+    front[0] = cos(pitch / 180 * M_PI) * cos(yaw / 180 * M_PI);
+    front[1] = sin(pitch / 180 * M_PI);
+    front[2] = cos(pitch / 180 * M_PI) * sin(yaw / 180 * M_PI);
+    // 默认朝向是世界Z轴负方向(不考虑俯仰)，yaw应该初始化为0
+    // front[0] = cos(pitch / 180 * M_PI) * sin(yaw / 180 * M_PI);
+    // front[1] = sin(pitch / 180 * M_PI);
+    // front[2] = -cos(pitch / 180 * M_PI) * cos(yaw / 180 * M_PI);
+    // 默认朝向是世界Z轴方向(不考虑俯仰)，yaw应该初始化为180
+    // front[0] = -cos(pitch / 180 * M_PI) * sin(yaw / 180 * M_PI);
+    // front[1] = sin(pitch / 180 * M_PI);
+    // front[2] = cos(pitch / 180 * M_PI) * cos(yaw / 180 * M_PI);
+
+    cam_front = front.normalized();
+
+    QCursor::setPos(mapToGlobal(QPoint(width() / 2, height() / 2)));
+
+    update();
+}
+
+void LzhOpenGLWidget::wheelEvent(QWheelEvent *event)
+{
+    QPoint numPixels = event->pixelDelta();
+    QPoint numDegrees = event->angleDelta() / 8;
+
+    if (!numPixels.isNull())
+    {
+        fov -= numPixels.y();
+    }
+    else if (!numDegrees.isNull()) {
+        QPoint numSteps = numDegrees / 15;
+        fov -= numSteps.y();
+    }
+    if(fov <= 1.0f)
+        fov = 1.0f;
+    if(fov >= 45.0f)
+        fov = 45.0f;
+
+    Perspective();
+    shader.Use();
+    shader.SetMat4("projection", perspective);
+    update();
+
+    event->accept();
+}
+
+void LzhOpenGLWidget::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key())
+    {
+    case Qt::Key_W:
+        cam_pos += cam_front * 0.2f;
+        update();
+        break;
+    case Qt::Key_S:
+        cam_pos -= cam_front * 0.2f;
+        update();
+        break;
+    case Qt::Key_A:
+        cam_pos -= QVector3D::crossProduct(cam_front, cam_up) * 0.2f;
+        update();
+        break;
+    case Qt::Key_D:
+        cam_pos += QVector3D::crossProduct(cam_front, cam_up) * 0.2f;
+        update();
+        break;
+    default:
+        break;
+    }
+}
+
+QMatrix4x4 LzhOpenGLWidget::LookAt(QVector3D &eye, const QVector3D &center, const QVector3D &up)
+{
+    QVector3D f = (center - eye).normalized();
+    QVector3D s = QVector3D::crossProduct(f, up).normalized();
+    QVector3D u = QVector3D::crossProduct(s, f);
+
+    QMatrix4x4 view;
+    view(0, 0) = s.x();
+    view(0, 1) = s.y();
+    view(0, 2) = s.z();
+    view(0, 3) = -QVector3D::dotProduct(s, eye);
+    view(1, 0) = u.x();
+    view(1, 1) = u.y();
+    view(1, 2) = u.z();
+    view(1, 3) = -QVector3D::dotProduct(u, eye);
+    view(2, 0) = -f.x();
+    view(2, 1) = -f.y();
+    view(2, 2) = -f.z();
+    view(2, 3) =  QVector3D::dotProduct(f, eye);
+    view(3, 0) = 0.0f;
+    view(3, 1) = 0.0f;
+    view(3, 2) = 0.0f;
+    view(3, 3) = 1.0f;
+
+    return view;
+}
+
+void LzhOpenGLWidget::Perspective()
+{
+    float scale = tan(fov * 0.5f * M_PI / 180) * 1.0f;
+    float imgAspectRatio = (float)width() / height();
+    float r = imgAspectRatio * scale;
+    float t = scale;
+
+    perspective(0, 0) = 1.0f / r;
+    perspective(0, 1) = 0.0f;
+    perspective(0, 2) = 0.0f;
+    perspective(0, 3) = 0.0f;
+
+    perspective(1, 0) = 0.0f;
+    perspective(1, 1) = 1.0f / t;
+    perspective(1, 2) = 0;
+    perspective(1, 3) = 0;
+
+    perspective(2, 0) = 0.0f;
+    perspective(2, 1) = 0.0f;
+    perspective(2, 2) = -(100.0f + 1.0f) / (100.0f - 1.0f);
+    perspective(2, 3) = -2 * 100.0 * 1.0 / (100.0f - 1.0f);
+
+    perspective(3, 0) = 0.0f;
+    perspective(3, 1) = 0.0f;
+    perspective(3, 2) = -1;
+    perspective(3, 3) = 0.0f;
+}
+
+QVector4D LzhOpenGLWidget::MakeQuaternion(QVector3D rotation_axis, double radian)
+{
+    double length = sqrt(rotation_axis[0] * rotation_axis[0] +
+                         rotation_axis[1] * rotation_axis[1] +
+                         rotation_axis[2] * rotation_axis[2]);
+
+    if (length < 1e-7) length = 1.0;
+
+    double inverse_norm = 1.0 / length;
+    double sin_half_angle = sin(0.5 * radian);
+    double cos_half_angle = cos(0.5 * radian);
+    double x = rotation_axis[0] * sin_half_angle * inverse_norm;
+    double y = rotation_axis[1] * sin_half_angle * inverse_norm;
+    double z = rotation_axis[2] * sin_half_angle * inverse_norm;
+    double w = cos_half_angle;
+
+    return QVector4D(x, y, z, w);
+}
+
+unsigned int LzhOpenGLWidget::LoadTexture(char const *path)
+{
+    unsigned int texture_ID;
+    glGenTextures(1, &texture_ID);
+
+    QImage image(path);
+
+    if (image.isNull())
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        return -1;
+    }
+
+    glBindTexture(GL_TEXTURE_2D, texture_ID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_BYTE, image.constBits());
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    return texture_ID;
+}
+
+unsigned int LzhOpenGLWidget::LoadCubemap(vector<std::string> faces)
+{
+    unsigned int texture_ID;
+    glGenTextures(1, &texture_ID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, texture_ID);
+
+    for (unsigned int i = 0; i < faces.size(); ++i)
+    {
+        QImage image(faces[i].c_str());
+
+        if (image.isNull())
+        {
+            std::cout << "Texture failed to load at path: " << faces[i] << std::endl;
+            return -1;
+        }
+        glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_BYTE, image.constBits());
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return texture_ID;
+}
---
+++ AdvancedOpenGL/6.1.Cubemaps_SkyBox/shader/6.1.cubemaps.fs
@@ -0,0 +1,10 @@
+#version 450 core
+
+in vec2 TexCoords;
+
+uniform sampler2D texture1;
+
+void main()
+{    
+    gl_FragColor = texture(texture1, TexCoords);
+}
