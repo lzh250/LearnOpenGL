799b33a4cbb42db63dcd04bb1c6ebbb12008afd4
lzh250 <lzh250@126.com>
2025-12-22 18:43:50 +0800

    阴影映射的深度贴图

---
+++ 5.AdvancedLighting/3.1.1.ShadewMappingDepth/lzhopenglwidget.cpp
@@ -0,0 +1,456 @@
+#include "lzhopenglwidget.h"
+
+#include <QKeyEvent>
+#include <QDateTime>
+#include <QOpenGLFramebufferObjectFormat>
+
+LzhOpenGLWidget::LzhOpenGLWidget(QWidget *parent) :
+    QOpenGLWidget(parent)
+{
+    setFocusPolicy(Qt::StrongFocus);
+    setMouseTracking(true);
+}
+
+LzhOpenGLWidget::~LzhOpenGLWidget()
+{
+    makeCurrent();
+    glDeleteVertexArrays(1, &plane_vao);
+    glDeleteBuffers(1, &plane_vbo);
+    doneCurrent();
+}
+
+void LzhOpenGLWidget::initializeGL()
+{
+    initializeOpenGLFunctions();
+
+    glEnable(GL_DEPTH_TEST);
+
+    cam_pos   = QVector3D(0.0f, 0.0f,  3.0f);
+    cam_front = QVector3D(0.0f, 0.0f, -1.0f);
+    cam_up    = QVector3D(0.0f, 1.0f,  0.0f);
+
+    simple_depth_shader.Init(":/shader/3.1.1.shadow_mapping_depth.vs", ":/shader/3.1.1.shadow_mapping_depth.fs");
+    debug_quad_shader.Init(":/shader/3.1.1.debug_quad.vs", ":/shader/3.1.1.debug_quad_depth.fs");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float plane_vertices[] = {
+        // positions            // normals         // texcoords
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+         25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
+    };
+
+    glGenVertexArrays(1, &plane_vao);
+    glBindVertexArray(plane_vao);
+    glGenBuffers(1, &plane_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, plane_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(plane_vertices), plane_vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void *)(sizeof(float) * 3));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void *)(sizeof(float) * 6));
+    glBindVertexArray(0);
+
+    float cube_vertices[] = {
+        // back face
+        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+        -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+        // front face
+        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+         1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+        -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+        // left face
+        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+        -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+        // right face
+         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+         1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+        // bottom face
+        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+        -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+        // top face
+        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+         1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right
+         1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+        -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left
+    };
+
+    glGenVertexArrays(1, &cube_vao);
+    glBindVertexArray(cube_vao);
+    glGenBuffers(1, &cube_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, cube_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void *)(sizeof(float) * 3));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void *)(sizeof(float) * 6));
+    glBindVertexArray(0);
+
+    glGenTextures(1, &depth_map_texture);
+    glBindTexture(GL_TEXTURE_2D, depth_map_texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glGenFramebuffers(1, &depth_map_fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, depth_map_fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_map_texture, 0);
+    glReadBuffer(GL_NONE);
+    glDrawBuffer(GL_NONE);
+    glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebufferObject());
+
+    debug_quad_shader.Use();
+    debug_quad_shader.SetInt("depthMap", 0);
+
+    light_pos = QVector3D(-2.0f, 4.0f, -1.0f);
+
+    float quad_vertices[] = {
+        // positions        // texture Coords
+        -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+         1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+         1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+    };
+    glGenVertexArrays(1, &quad_vao);
+    glBindVertexArray(quad_vao);
+    glGenBuffers(1, &quad_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, quad_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertices), quad_vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void *)(sizeof(float) * 3));
+
+    glBindVertexArray(0);
+}
+
+void LzhOpenGLWidget::resizeGL(int w, int h)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, w, h);
+    // Perspective();
+    // debug_quad_shader.Use();
+    // debug_quad_shader.SetMat4("projection", perspective);
+}
+
+void LzhOpenGLWidget::paintGL()
+{
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    QMatrix4x4 light_projection;
+    float near_plane = 1.0f, far_plane = 7.5f;
+    light_projection.ortho(-10, 10, -10, 10, near_plane, far_plane);
+    QMatrix4x4 light_view = LookAt(light_pos, QVector3D(0.0f, 0.0f, 0.0f), QVector3D(0.0f, 1.0f, 0.0f));
+    QMatrix4x4 light_space_matrix = light_projection * light_view;
+
+    simple_depth_shader.Use();
+    simple_depth_shader.SetMat4("lightSpaceMatrix", light_space_matrix);
+    glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+    glBindFramebuffer(GL_FRAMEBUFFER, depth_map_fbo);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    RenderScene(simple_depth_shader);
+    glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebufferObject());
+
+    glViewport(0, 0, width(), height());
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    debug_quad_shader.Use();
+    debug_quad_shader.SetFloat("near_plane", near_plane);
+    debug_quad_shader.SetFloat("far_plane", far_plane);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, depth_map_texture);
+    RenderQuad();
+}
+
+void LzhOpenGLWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    QPoint pos = event->pos();
+
+    if (first_mouse)
+    {
+        QCursor::setPos(mapToGlobal(QPoint(width() / 2, height() / 2)));
+        first_mouse = false;
+        return;
+    }
+
+    float x_offset = pos.x() - width()/2;
+    float y_offset = height()/2 - pos.y();
+
+    x_offset *= 0.05f;
+    y_offset *= 0.05f;
+
+    yaw   += x_offset;
+    pitch += y_offset;
+
+    if (pitch >  89.0f) pitch =  89.0f;
+    if (pitch < -89.0f) pitch = -89.0f;
+
+    QVector3D front;
+    // 默认朝向是世界X轴方向(不考虑俯仰)，yaw应该初始化为-90
+    front[0] = cos(pitch / 180 * M_PI) * cos(yaw / 180 * M_PI);
+    front[1] = sin(pitch / 180 * M_PI);
+    front[2] = cos(pitch / 180 * M_PI) * sin(yaw / 180 * M_PI);
+    // 默认朝向是世界Z轴负方向(不考虑俯仰)，yaw应该初始化为0
+    // front[0] = cos(pitch / 180 * M_PI) * sin(yaw / 180 * M_PI);
+    // front[1] = sin(pitch / 180 * M_PI);
+    // front[2] = -cos(pitch / 180 * M_PI) * cos(yaw / 180 * M_PI);
+    // 默认朝向是世界Z轴方向(不考虑俯仰)，yaw应该初始化为180
+    // front[0] = -cos(pitch / 180 * M_PI) * sin(yaw / 180 * M_PI);
+    // front[1] = sin(pitch / 180 * M_PI);
+    // front[2] = cos(pitch / 180 * M_PI) * cos(yaw / 180 * M_PI);
+
+    cam_front = front.normalized();
+
+    QCursor::setPos(mapToGlobal(QPoint(width() / 2, height() / 2)));
+
+    update();
+}
+
+void LzhOpenGLWidget::wheelEvent(QWheelEvent *event)
+{
+    QPoint numPixels = event->pixelDelta();
+    QPoint numDegrees = event->angleDelta() / 8;
+
+    if (!numPixels.isNull())
+    {
+        fov -= numPixels.y();
+    }
+    else if (!numDegrees.isNull()) {
+        QPoint numSteps = numDegrees / 15;
+        fov -= numSteps.y();
+    }
+    if(fov <= 1.0f)
+        fov = 1.0f;
+    if(fov >= 45.0f)
+        fov = 45.0f;
+
+    //Perspective();
+    // shader.Use();
+    // shader.SetMat4("projection", perspective);
+    update();
+
+    event->accept();
+}
+
+void LzhOpenGLWidget::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key())
+    {
+    case Qt::Key_W:
+        cam_pos += cam_front * 0.2f;
+        update();
+        break;
+    case Qt::Key_S:
+        cam_pos -= cam_front * 0.2f;
+        update();
+        break;
+    case Qt::Key_A:
+        cam_pos -= QVector3D::crossProduct(cam_front, cam_up) * 0.2f;
+        update();
+        break;
+    case Qt::Key_D:
+        cam_pos += QVector3D::crossProduct(cam_front, cam_up) * 0.2f;
+        update();
+        break;
+    // case Qt::Key_B:
+    // {
+    //     //*************************************************************
+    //     // Qt长期按住一个键再释放时的事件特性：
+    //     // 1. 触发KeyPressEvent，isAutoRepeat()返回false
+    //     // 2. 停顿一会儿
+    //     // 3. 触发KeyPressEvent，isAutoRepeat()返回true
+    //     // 4. 触发KeyReleaseEvent，isAutoRepeat()返回true
+    //     // 5. 3 4 循环往复
+    //     // 6. 释放按键，触发KeyReleaseEvent，isAutoRepeat()返回false
+    //     //*************************************************************
+    //     if (!event->isAutoRepeat())
+    //     {
+    //         if (b_key_released)
+    //         {
+    //             gamma_enabled = !gamma_enabled;
+    //         }
+    //         b_key_released = false;
+    //         update();
+    //     }
+    // }
+    //     break;
+    default:
+        break;
+    }
+}
+
+void LzhOpenGLWidget::keyReleaseEvent(QKeyEvent *event)
+{
+    // if (event->key() == Qt::Key_B && !event->isAutoRepeat())
+    // {
+    //     b_key_released = true;
+    // }
+}
+
+void LzhOpenGLWidget::RenderScene(Shader &shader)
+{
+    // floor
+    QMatrix4x4 model;
+    shader.SetMat4("model", model);
+    glBindVertexArray(plane_vao);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    //cubes
+    model.translate(0.0f, 1.5f, 0.0);
+    model.scale(0.5f);
+    shader.SetMat4("model", model);
+    RanderCube();
+    model.setToIdentity();
+    model.translate(2.0f, 0.0f, 1.0);
+    model.scale(0.5f);
+    shader.SetMat4("model", model);
+    RanderCube();
+    model.setToIdentity();
+    model.translate(-1.0f, 0.0f, 2.0);
+    model.rotate(60.0f, 1.0f, 0.0f, 1.0f);
+    model.scale(0.25f);
+    shader.SetMat4("model", model);
+    RanderCube();
+}
+
+void LzhOpenGLWidget::RanderCube()
+{
+    glBindVertexArray(cube_vao);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+void LzhOpenGLWidget::RenderQuad()
+{
+    glBindVertexArray(quad_vao);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+
+QMatrix4x4 LzhOpenGLWidget::LookAt(QVector3D &eye, const QVector3D &center, const QVector3D &up)
+{
+    QVector3D f = (center - eye).normalized();
+    QVector3D s = QVector3D::crossProduct(f, up).normalized();
+    QVector3D u = QVector3D::crossProduct(s, f);
+
+    QMatrix4x4 view;
+    view(0, 0) = s.x();
+    view(0, 1) = s.y();
+    view(0, 2) = s.z();
+    view(0, 3) = -QVector3D::dotProduct(s, eye);
+    view(1, 0) = u.x();
+    view(1, 1) = u.y();
+    view(1, 2) = u.z();
+    view(1, 3) = -QVector3D::dotProduct(u, eye);
+    view(2, 0) = -f.x();
+    view(2, 1) = -f.y();
+    view(2, 2) = -f.z();
+    view(2, 3) =  QVector3D::dotProduct(f, eye);
+    view(3, 0) = 0.0f;
+    view(3, 1) = 0.0f;
+    view(3, 2) = 0.0f;
+    view(3, 3) = 1.0f;
+
+    return view;
+}
+
+void LzhOpenGLWidget::Perspective()
+{
+    float scale = tan(fov * 0.5f * M_PI / 180) * 1.0f;
+    float imgAspectRatio = (float)width() / height();
+    float r = imgAspectRatio * scale;
+    float t = scale;
+
+    perspective(0, 0) = 1.0f / r;
+    perspective(0, 1) = 0.0f;
+    perspective(0, 2) = 0.0f;
+    perspective(0, 3) = 0.0f;
+
+    perspective(1, 0) = 0.0f;
+    perspective(1, 1) = 1.0f / t;
+    perspective(1, 2) = 0;
+    perspective(1, 3) = 0;
+
+    perspective(2, 0) = 0.0f;
+    perspective(2, 1) = 0.0f;
+    perspective(2, 2) = -(far + near) / (far - near);
+    perspective(2, 3) = -2 * far * near / (far - near);
+
+    perspective(3, 0) = 0.0f;
+    perspective(3, 1) = 0.0f;
+    perspective(3, 2) = -1;
+    perspective(3, 3) = 0.0f;
+}
+
+QVector4D LzhOpenGLWidget::MakeQuaternion(QVector3D rotation_axis, double radian)
+{
+    double length = sqrt(rotation_axis[0] * rotation_axis[0] +
+                         rotation_axis[1] * rotation_axis[1] +
+                         rotation_axis[2] * rotation_axis[2]);
+
+    if (length < 1e-7) length = 1.0;
+
+    double inverse_norm = 1.0 / length;
+    double sin_half_angle = sin(0.5 * radian);
+    double cos_half_angle = cos(0.5 * radian);
+    double x = rotation_axis[0] * sin_half_angle * inverse_norm;
+    double y = rotation_axis[1] * sin_half_angle * inverse_norm;
+    double z = rotation_axis[2] * sin_half_angle * inverse_norm;
+    double w = cos_half_angle;
+
+    return QVector4D(x, y, z, w);
+}
+
+unsigned int LzhOpenGLWidget::LoadTexture(const char *path)
+{
+    QImage image(path);
+    if (image.isNull())
+    {
+        qDebug() << "Failed to open the image: " << path;
+        return -1;
+    }
+
+    unsigned int texture_id;
+    glGenTextures(1, &texture_id);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_BYTE, image.mirrored().constBits());
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT); // for this tutorial: use GL_CLAMP_TO_EDGE to prevent semi-transparent borders. Due to interpolation it takes texels from next repeat
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    return texture_id;
+}
