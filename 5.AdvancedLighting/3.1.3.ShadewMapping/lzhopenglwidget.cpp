8cc46d51b1f927881388a79178b92e9a295ec073
lzh250 <lzh250@126.com>
2025-12-24 12:44:08 +0800

    改进阴影映射-通过GL_CLAMP_TO_BORDER方式解决超出光视锥以外的部分被阴影覆盖问题

--- 5.AdvancedLighting/3.1.3.ShadewMapping/lzhopenglwidget.cpp
+++ 5.AdvancedLighting/3.1.3.ShadewMapping/lzhopenglwidget.cpp
@@ -124,8 +124,10 @@ void LzhOpenGLWidget::initializeGL()
     glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    float borderColor[] = {1.0, 1.0, 1.0, 1.0};
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
 
     glGenFramebuffers(1, &depth_map_fbo);
     glBindFramebuffer(GL_FRAMEBUFFER, depth_map_fbo);
@@ -188,13 +190,8 @@ void LzhOpenGLWidget::paintGL()
 
     glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
     glBindFramebuffer(GL_FRAMEBUFFER, depth_map_fbo);
-    glEnable(GL_CULL_FACE);
     glClear(GL_DEPTH_BUFFER_BIT);
-    /////// glActiveTexture(GL_TEXTURE0);
-    /////// glBindTexture(GL_TEXTURE_2D, wood_texture);
-    glCullFace(GL_FRONT);
     RenderScene(simple_depth_shader);
-    glCullFace(GL_BACK);
 
     glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebufferObject());
     glViewport(0, 0, width(), height());
