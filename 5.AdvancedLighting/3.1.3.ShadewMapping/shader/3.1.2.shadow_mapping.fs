b4c914307a357edd5614400a092991a7a0acc01e
lzh250 <lzh250@126.com>
2025-12-24 12:56:45 +0800

    改进阴影映射-解决投影向量z坐标大于1.0被阴影覆盖问题。此处理使阴影只会出现在深度贴图的覆盖范围内，而光源视锥体以外的区域不会产生阴影，远处是否实际有阴影就不管了，很多情况下这种体验效果不错。

--- 5.AdvancedLighting/3.1.3.ShadewMapping/shader/3.1.2.shadow_mapping.fs
+++ 5.AdvancedLighting/3.1.3.ShadewMapping/shader/3.1.2.shadow_mapping.fs
@@ -14,7 +14,7 @@ uniform sampler2D shadowMap;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
-float ShadowCalculation(vec4 fragPosLightSpace, vec3 normal, vec3 lightDir)
+float ShadowCalculation(vec4 fragPosLightSpace, vec3 normal)
 {
     // 当我们在顶点着色器输出一个裁切空间顶点位置到gl_Position时，OpenGL自动进行一个透视除法，例如，将裁切空间坐标的范围[-w,w]转为[-1,1]，这要将x、y、z分量除以向量的w分量来实现。由于裁切空间的fragPosLightSpace并不会通过gl_Position传到片段着色器里，我们必须自己做透视除法
     // 下面过程首先通过除法进行统一，然后将结果 * 0.5 + 0.5 将[-1,1]范围的数据转换成对应的纹理位置[0, 1]数据，用于从深度纹理图片中提取对应值。
@@ -27,8 +27,13 @@ float ShadowCalculation(vec4 fragPosLightSpace, vec3 normal, vec3 lightDir)
     // get depth of current fragment from light's perspective
     float currentDepth = projCoords.z;
     // check whether current frag pos is in shadow
-    float bias = max(0.05 * (1 - dot(normal, lightDir)), 0.005);
+    float bias = max(0.05 * (1 - dot(normal, lightPos)), 0.005);
     float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+
+    if (projCoords.z > 1.0)
+    {
+        shadow = 0.0;
+    }
 
     return shadow;
 }
@@ -51,7 +56,7 @@ void main()
     spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
     vec3 specular = spec * lightColor;    
     // calculate shadow
-    float shadow = ShadowCalculation(fs_in.FragPosLightSpace, normal, lightPos);
+    float shadow = ShadowCalculation(fs_in.FragPosLightSpace, normal);
     vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
     
     FragColor = vec4(lighting, 1.0);
