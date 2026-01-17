7fe293556799568109bbd090618ac5eab38261a5
lzh250 <lzh250@126.com>
2026-01-17 23:45:13 +0800

    泛光（第一步）

---
+++ 5.AdvancedLighting/7.1.Bloom/lzhopenglwidget.h
@@ -0,0 +1,68 @@
+#ifndef LZHOPENGLWIDGET_H
+#define LZHOPENGLWIDGET_H
+
+#include "shader.h"
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_4_5_Core>
+#include <QMatrix4x4>
+
+class LzhOpenGLWidget : public QOpenGLWidget, QOpenGLFunctions_4_5_Core
+{
+public:
+    LzhOpenGLWidget(QWidget *parent);
+    virtual ~LzhOpenGLWidget();
+
+    // QOpenGLWidget interface
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+
+    // QWidget interface
+protected:
+    void mouseMoveEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+    void keyReleaseEvent(QKeyEvent *event);
+
+private:
+    void RenderCube();
+    void RenderQuad();
+
+    QMatrix4x4 LookAt( QVector3D &eye, const QVector3D &center, const QVector3D &up);
+    QMatrix4x4 Perspective(float fov, float aspect, float near, float far);
+    QVector4D MakeQuaternion(QVector3D rotation_axis, double radian);
+
+    unsigned int LoadTexture(const char *path);
+
+private:
+    Shader       shader, shader_light, shader_bloom_final;
+    unsigned int wood_texture, container_exture;
+    unsigned int hdr_fbo;
+    unsigned int color_buffer;
+    unsigned int rbo_depth;
+    QVector<QVector3D> light_positions;
+    QVector<QVector3D> light_colors;
+    unsigned int cube_vao, cube_vbo;
+    unsigned int quad_vao, quad_vbo;
+
+    // bool         b_key_released = true;
+    float        exposure = 1.0f;
+
+    // for LookAt
+    QVector3D cam_pos;
+    QVector3D cam_front;
+    QVector3D cam_up;
+
+    QMatrix4x4 perspective;
+    float far = 100.0f;
+    float near = 0.1f;
+    float fov = 45.0f;
+
+    bool first_mouse = true;
+    float yaw = -90.0f;
+    float pitch = 0.0f;
+};
+
+#endif // LZHOPENGLWIDGET_H
---
+++ 5.AdvancedLighting/7.1.Bloom/shader/7.bloom.fs
@@ -0,0 +1,40 @@
+#version 450 core
+out vec4 FragColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} fs_in;
+
+struct Light {
+    vec3 Position;
+    vec3 Color;
+};
+
+uniform Light lights[4];
+uniform sampler2D diffuseTexture;
+
+void main()
+{           
+    vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
+    vec3 normal = normalize(fs_in.Normal);
+    // ambient
+    vec3 ambient = 0.0 * color;
+    // lighting
+    vec3 lighting = vec3(0.0);
+    for(int i = 0; i < 4; i++)
+    {
+        // diffuse
+        vec3 lightDir = normalize(lights[i].Position - fs_in.FragPos);
+        float diff = max(dot(lightDir, normal), 0.0);
+        vec3 result = lights[i].Color * diff * pow(color, vec3(2.2f));
+        // attenuation (use quadratic as we have gamma correction)
+        float distance = length(fs_in.FragPos - lights[i].Position);
+        result *= 1.0 / (distance * distance);
+        lighting += result;
+                
+    }
+    vec3 result = ambient + lighting;
+    FragColor = vec4(result, 1.0);
+}
